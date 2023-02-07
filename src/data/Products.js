88e13d4fa801510edda18055df5a8f0c91114509
marionnevq <quintanamarionne@gmail.com>
2023-02-07 14:38:59 +0800

    added product data

---
+++ src/data/Products.js
@@ -0,0 +1,62 @@
+export const products = [
+  {
+    id: 1000,
+    name: "PlayStation 5 Disk Edition",
+    price: 27000.0,
+    stock: 25,
+  },
+  {
+    id: 1001,
+    name: "Dualsense Controller Black",
+    price: 3500.0,
+    stock: 15,
+  },
+  {
+    id: 1002,
+    name: "Dualsense Controller White",
+    price: 3500.0,
+    stock: 10,
+  },
+  {
+    id: 1003,
+    name: "Pulse 3D Wireless Headset",
+    price: 5395.0,
+    stock: 8,
+  },
+  {
+    id: 1004,
+    name: "PS5 HD Camera",
+    price: 3250.0,
+    stock: 13,
+  },
+  {
+    id: 1005,
+    name: "Nyko PS5 Charge Base",
+    price: 1100.0,
+    stock: 7,
+  },
+  {
+    id: 1006,
+    name: "Skull & Co PS5 Thumbgrips",
+    price: 190.0,
+    stock: 5,
+  },
+  {
+    id: 1007,
+    name: "Hori Fighting Stick Alpha",
+    price: 8450.0,
+    stock: 3,
+  },
+  {
+    id: 1008,
+    name: "Razer Barracuda X Wireless Headset Pink",
+    price: 5495.0,
+    stock: 5,
+  },
+  {
+    id: 1009,
+    name: "PS5 Protective Shell Purple",
+    price: 3090.0,
+    stock: 3,
+  },
+];
